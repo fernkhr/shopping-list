90788d5190acdbbae9754c72b41b797e94aa6434
fernkhr <fern.khairunnisha@ui.ac.id>
2023-09-13 19:51:56 +0700

    tutorial 2 sementara

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main.views import show_main
 
 urlpatterns = [
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
 ]
